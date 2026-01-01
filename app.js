11cd6da566a77b430c682d52efe429c65adf4e39
LIGONE-token <rene.herrchen@gmail.com>
2026-01-01 15:40:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -203,9 +203,6 @@ async function loadRelatedEntries(entry) {
   `entries_with_ratings?select=id,title,summary,score,processing_score,rating_avg,rating_count&category=eq.${encodeURIComponent(cleanCategory)}&id=neq.${cleanId}&limit=5`
 );
 
-
-  );
-
   if (!data.length) return "";
 
   return `
